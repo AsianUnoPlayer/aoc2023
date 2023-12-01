259d6359a8bbb813e06588164db7c31946fcefda
AsianUnoPlayer <asianunoplayer@gmail.com>
2023-12-01 17:59:31 -0500

    revamped/completed Part2

--- Day1/day1.cpp
+++ Day1/day1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
 using namespace std;
 
-int final_output = 0;
-string vocab[9] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+int final_number = 0, final_output = 0;
+string vocab[18] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 void PartOne(string text)
 {
@@ -25,9 +26,43 @@ void PartOne(string text)
 	}
 	number = stoi(lineNumber);
 
-	final_output += number;
-	//cout << "Line is: " << number << endl;
+	final_number += number;
 
+}
+
+void PartTwo(string text)
+{
+	int number;
+	string lineOutput;
+
+	map<int, string> numberValues;
+
+	for (int x = 0; x < sizeof(vocab)/sizeof(vocab[0]); x++) {
+		int currentPos = text.find(vocab[x]);
+
+		if (currentPos != -1) {
+			int newPos = text.rfind(vocab[x]);
+
+			if (newPos != -1 && newPos > currentPos) {
+				currentPos = newPos;
+			}
+
+			if (x <= 8) {
+				numberValues.insert({currentPos, vocab[x+9]});
+			}
+			else {
+				numberValues.insert({currentPos, vocab[x]});
+			}
+		}
+	}
+
+	lineOutput += numberValues.begin()->second;
+	auto it = numberValues.end(); it--;
+	lineOutput += it->second;
+
+	number = stoi(lineOutput);
+	final_output += number;
+	numberValues.clear();
 }
 
 int main()
@@ -37,20 +72,12 @@ int main()
 	input_file.open("input.txt");
 
 	while (getline(input_file, text)) {
-		int number;
-		string lineOutput;
-
-		for (int x = 0; x < sizeof(vocab)/sizeof(vocab[0]); x++) {
-			//text.find(vocab[x])
-			//text.find(vocab[x], 0)
-			if (text.find(vocab[x], 0) != -1) {
-				cout << vocab[x] << " ";
-			}
-		}
-		cout << endl;
+		PartOne(text);
+		PartTwo(text);
 	}
 
-	cout << "Final Output is: " << final_output << endl;
+	cout << "PartOne is: " << final_number << endl;
+	cout << "PartTwo is: " << final_output << endl;
 
 	return 0;
 }
